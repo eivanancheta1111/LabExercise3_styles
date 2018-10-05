d8525391fdf2cf9be66b7ad5d07527190ef5d85a
eivanancheta1111 <eivan.ancheta@gmail.com>
2018-10-05 22:03:38 +0800

    Initial commit

---
+++ app/src/main/java/iics/ancheta/jeanroy/labexercise3_styles/MainActivity.java
@@ -0,0 +1,13 @@
+package iics.ancheta.jeanroy.labexercise3_styles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
